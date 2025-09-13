9ed71eca59091e2f220a08b0e66c264051191dce
jahaa032 <jahaa032@osloskolen.no>
2025-09-13 12:57:52 +0200

    Falsk valg

--- avstemningssystem.py
+++ avstemningssystem.py
@@ -1,3 +1,4 @@
+import random
 def vis_partier(partier):
     print("Du kan stemme på disse partiende: ")
     for parti in partier:
@@ -22,8 +23,32 @@ def resultat(stemmer):
     print("\n--- Stemme resultat ---")
     for parti, antall in stemmer.items():
         print(f"{parti}: {antall} stemmer")
+        
+def vis_prosent(stemmer):
+    print("\n--- Stemme resultat ---")
+    total = sum(stemmer.values())
+    for parti, antall in stemmer.items():
+       prosent = (antall / total) * 100
+       print(f"{parti}: {prosent:.1f}%")
+        # print(f"{parti}: {antall} {prosent} stemmer")
+
+def falskt_valg(partier, stemmer):
+    weights = [0.31, 0.14, 0.27, 0.053, 0.053, 0.017, 0.041, 0.047, 0.053 ]
+    random_valg = random.choices(partier, weights=weights, k=1)[0]
+    stemmer[random_valg] + 1
     
-def hovedprgram():
+    for stemmer in partier:
+        if random_valg >= 40:
+            print(f"Stemmene er telt og {random_valg} Vant")
+            return False
+        else:
+            return True
+
+
+    # votes = random.choices(random_valg, num_choices)
+    # print(f"Valget endte opp med: {votes}")
+
+def hovedporgram():
     partier = [
         "ap",
         "høyre",
@@ -41,65 +66,10 @@ def hovedprgram():
     vis_partier(partier)
 
     while True:
-        fortsett = sjekk_stemmer(partier, stemmer)
+        fortsett = falskt_valg(partier, stemmer)
         if not fortsett:
             break
 
     resultat(stemmer)
 
-hovedprgram()
-
-
-
-
-# print("Disse er partiende du kan stemme på: ")
-# for p in partier:
-#     print("-", p)
-
-# while True:
-#     stemme = input("Hvem vil du stemme på av partiende?: ").strip().lower()
-
-#     if stemme in p:
-#         partier[stemme] += 1
-#         print(f"Du stemte på {stemme}")
-#     else:
-#         print("Du kan ikke stemme på det, prøv igjen.")
-
-
-
-
-
-
-# def stemmer():
-#     spørsmål = input("Hvem vil du stemme på?: 0 for høyre, 1 for frp, 2 for venstre, 3 for arbeiderpartiet, 4 for sv, 5 for rødt, 6 for mdg, 7 for krf, 8 for senterpartiet")
-#     parti = ["høyre", "frp", "venstre", "arbeiderpartiet", "sv", "rødt", "mdg", "krf", "senterpartiet"]
-#     stemmer_id = [1,2,3,4,5,6,7,8,9]
-#     # num_of_stemmer = len(stemmer_id)
-
-#     stemmning = list(zip(parti,stemmer_id))
-#     hvilke_stemmer = list(zip(spørsmål, stemmning))
-#     for spørsmål, stemmer_id in hvilke_stemmer:
-#         print(spørsmål)
-
-#     while True:
-#         if stemmer_id == []:
-#             print("Ferdig med å stemme")
-#             if hvilke_stemmer
-            
-#             # stemme = int("skriv in hvem du vil stemme på: ")
-#             # if stemme in parti:
-#             # print("Du har valgt ditt parti")
-#             # num_of_stemmer.remove()
-#             # return stemme
-        
-
-
-
-
-#  # for stemme in parti:
-#     #     print(stemme)
-#     #     bruker_stemme = input("Din stemme: ").lower().strip()
-
-#     #     if bruker_stemme() == parti.lower():
-#     #         print("Din stemme har blitt lagret.")
-#     #         return bruker_stemme
+hovedporgram()
