a2836c8e1f9dbdca9737e0ae61d84024483e30e4
jahaa032 <jahaa032@osloskolen.no>
2025-09-14 00:05:05 +0200

    Last update

--- avstemningssystem.py
+++ avstemningssystem.py
@@ -1,4 +1,12 @@
 import random
+import json
+
+def lagre_valg(navn, stemmer):
+    filnavn = f"{navn}_resultater.json"
+    with open(filnavn, "w") as fil:
+        json.dump(stemmer, fil, indent=4)
+        print(f"Resultatene dine er lagret i {filnavn}")
+
 def vis_partier(partier):
     print("Du kan stemme på disse partiende: ")
     for parti in partier:
@@ -34,39 +42,22 @@ def falskt_valg(partier, stemmer):
     ]
     weights = [random.uniform(low, high) for low, high in intervaller]
     random_valg = random.choices(partier, weights=weights, k=1)[0]
-    stemmer[random_valg] + 1
-    print(f"Stemmene er telt og {random_valg} Vant, {weights}")
+    stemmer[random_valg] += 1
+    print(f"Stemmene er telt og {random_valg}")
 
     return True
 
 def resultat(stemmer):
     print("\n--- Stemme resultat ---")
     for parti, antall in stemmer.items():
-        print(f"{parti:.2f}: {antall} stemmer")
+        print(f"{parti}: {antall} stemmer")
         
 def vis_prosent(stemmer):
-    print("\n--- Stemme resultat ---")
+    print("\n--- Stemme resultat Prosenter ---")
     total = sum(stemmer.values())
     for parti, antall in stemmer.items():
        prosent = (antall / total) * 100
        print(f"{parti}: {prosent:.1f}%")
-        # print(f"{parti}: {antall} {prosent} stemmer")
-
-
-# if stemme.lower() == 'q':
-#         print("Hadde")
-#         return False
-
-#     if stemme in partier:
-#         stemmer[stemme] += 1
-#         print(f"Du har stemt på {stemme}!\n")
-#     else:
-#         print("Du kan ikke stemme på det, prøv igjen.\n")
-    
-#     return True
-        
-    # votes = random.choices(random_valg, num_choices)
-    # print(f"Valget endte opp med: {votes}")
 
 def hovedporgram():
     partier = [
@@ -85,14 +76,21 @@ def hovedporgram():
     print("Velkomen til valget")
     vis_partier(partier)
 
-    while True:
-        fortsett = falskt_valg(partier, stemmer)
-        if not fortsett:
-           break
+    # while True:
+    #     fortsett = falskt_valg(partier, stemmer)
+    #     if fortsett <= 40:
+    #         return True
+    #     elif not fortsett:
+    #        break
 
-    while resultat <= 169:
-        stemmer
+    for i in range(169):
+        falskt_valg(partier, stemmer)
+        
         
     resultat(stemmer)
+    vis_prosent(stemmer)
+
+    navn = input("Skriv in navnet ditt: ")
+    lagre_valg(navn, stemmer)
 
 hovedporgram()
